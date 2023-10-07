c5cd8b4c292a5163f998ab47188f82b766217897
Vedant <vedant7978@gmail.com>
2023-10-07 18:14:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -46,10 +46,10 @@ function App() {
     <Switch>
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          <Route exact path="/about">
+          <Route exact path="/textutils/about">
             <About mode={mode} />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/textutils">
             <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
           </Route>
     </Switch>
